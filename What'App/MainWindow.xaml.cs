e642f52a0fc24ab35cb1e9a529d235660981f1ba
MA <kflwwa@gmail.com>
2025-05-14 22:22:52 +0900

    2

--- What'App/MainWindow.xaml.cs
+++ What'App/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoUpdaterDotNET;
 
 namespace What_App
 {
@@ -19,8 +20,13 @@ namespace What_App
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+            Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            AutoUpdater.Start("");
+        }
         private void button_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
